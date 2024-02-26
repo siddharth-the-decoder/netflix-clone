340cd0c8041a4b32a2e4189c81a93e14cdb6358e
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-02-26 22:19:24 +0530

    auth

---
+++ pages/auth.tsx
@@ -0,0 +1,22 @@
+import Input from "@/components/Input";
+const Auth = () => {
+  return (
+    <div className="relative min-h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full min-h-screen lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
