4992a54e6c3c7ae772828c7f42f000d1720a179d
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-02-27 08:56:49 +0530

    auth2

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,23 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({id, onChange, value, label, type}) => {
   return (
     <div className="relative">
       <input
-        id="email"
+        value={value}
+        onChange={onChange}
+        type={type}
+        id={id}
         className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white  bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"
-        placeholder=" "
+        placeholder="  "
       />
       <label
         className="absolute 
@@ -21,9 +34,9 @@ const Input = () => {
            peer-placeholder-shown:translate-y-0 
             peer-focus:scale-75 
             peer-focus:-translate-y-3"
-        htmlFor="email"
+        htmlFor={id}
       >
-        Email
+        {label}
       </label>
     </div>
   );
